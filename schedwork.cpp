b9e4e75e8aedee8f115e4ef500b443fce427ca7c
Harman Pelia <pelia@usc.edu>
2023-04-12 23:52:17 -0700

    working state

--- schedwork.cpp
+++ schedwork.cpp
@@ -8,37 +8,53 @@
 #include <algorithm>
 #endif
 
-// add or remove necessary headers as you please
 #include "schedwork.h"
 
 using namespace std;
 
-// a constant that can be used to indicate an INVALID 
-// worker ID if that is useful to your implementation.
-// Feel free to not use or delete.
 static const Worker_T INVALID_ID = (unsigned int)-1;
 
-
 // Add prototypes for any helper functions here
-
+bool verification(DailySchedule& sched, const AvailabilityMatrix& avail, vector<size_t>& shiftCounts, const size_t dailyNeed, const size_t maxShifts, size_t d_, size_t w_);
 
 // Add your implementation of schedule() and other helper functions here
-
 bool schedule(
     const AvailabilityMatrix& avail,
     const size_t dailyNeed,
     const size_t maxShifts,
     DailySchedule& sched
-)
-{
-    if(avail.size() == 0U){
-        return false;
-    }
+) {
+    if (avail.empty()) return false;
     sched.clear();
-    // Add your code below
 
+    std::vector<size_t> shiftCounts(avail[0].size(), 0);
+    sched.resize(avail.size(), std::vector<Worker_T>(dailyNeed));
 
-
-
+    return verification(sched, avail, shiftCounts, dailyNeed, maxShifts, 0, 0);
 }
 
+bool verification(DailySchedule& sched, const AvailabilityMatrix& avail, std::vector<size_t>& shiftCounts, const size_t dailyNeed, const size_t maxShifts, size_t d_, size_t w_ ) {
+    
+    //base case
+    if (d_ == avail.size()) return true;
+
+    if (w_ == dailyNeed) return verification(sched, avail, shiftCounts, dailyNeed, maxShifts, d_ + 1, 0);
+
+    
+    for (int i = 0; i < avail[d_].size(); i++) {
+
+        if (avail[d_][i] && shiftCounts[i] < maxShifts) {
+
+            sched[d_][w_] = i;
+            shiftCounts[i]++;
+
+            if (verification(sched, avail, shiftCounts, dailyNeed, maxShifts, d_, w_ + 1)) return true;
+
+            sched[d_][w_] = INVALID_ID;
+            shiftCounts[i]--;
+            
+        }
+    }
+
+    return false;
+}
--- wordle.cpp
+++ wordle.cpp
@@ -13,6 +13,15 @@ using namespace std;
 
 
 // Add prototypes of helper functions here
+bool existing(const string& word, const set<string>& dict);
+
+void combine(const std::string& pattern, 
+    const string& floating, 
+    std::set<std::string>& results, 
+    int index, 
+    const std::set<std::string>& dict);
+
+
 
 
 // Definition of primary wordle function
@@ -22,7 +31,48 @@ std::set<std::string> wordle(
     const std::set<std::string>& dict)
 {
     // Add your code here
+	std::set<std::string> final;
 
+	combine(in, floating, final, 0, dict);
+
+	return final;
 }
 
 // Define any helper functions here
+
+void combine(const std::string& pattern, const std::string& floating, std::set<std::string>& final, int index, const std::set<std::string>& dict){
+
+    if (index == pattern.size()){
+
+        if (existing(pattern, dict)) final.insert(pattern);
+
+        return;
+    }
+
+    if (pattern[index] == '-'){
+
+        for (char c : floating) {
+
+            std::string newPattern_ = pattern;
+
+            newPattern_[index] = c;
+
+            std::string newfloat_ = floating;
+
+            auto pos = newfloat_.find(c);
+
+            if (pos != std::string::npos) newfloat_.erase(pos, 1);
+    
+            combine(newPattern_, newfloat_, final, index+1, dict);
+        }
+    }
+
+    else{
+        combine(pattern, floating, final, index+1, dict);
+    }
+}
+
+
+bool existing(const std::string& word, const std::set<std::string>& dict){
+	return dict.find(word) != dict.end();
+}
